eb74b79e3311e0079960e9a8630e9b53f17e835f
Riccardo Di Nuzzo <riccardo@dinuzzo.it>
2014-09-18 04:09:52 -0500

    start configuring settings and layout

--- StudentHelper/scripts/app/settings.js
+++ StudentHelper/scripts/app/settings.js
@@ -5,7 +5,7 @@
 var appSettings = {
 
     everlive: {
-        apiKey: '$EVERLIVE_API_KEY$', // Put your Backend Services API key here
+        apiKey: 'iJIoDMGc0yja6hrP', // Put your Backend Services API key here
         scheme: 'http'
     },
 
