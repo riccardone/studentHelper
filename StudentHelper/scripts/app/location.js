04b9b11782280158821a56604ab0a9ad93a97884
Riccardo Di Nuzzo <riccardo@dinuzzo.it>
2014-10-01 09:08:38 -0500

    added activities example

--- StudentHelper/scripts/app/location.js
+++ StudentHelper/scripts/app/location.js
@@ -138,73 +138,11 @@
             var school = app.Schools.schools.getByUid(e.view.params.uid);
             var lat = e.view.params.lat;            
             var lon = e.view.params.lon;            
-            school.distance = dataContext.calculateDistance({lat: school.location.Latitude, lon:school.location.Longitude}, {lat: lat, lon: lon});            
-            //onNavigateHome
+            school.distance = dataContext.calculateDistance({lat: school.location.Latitude, lon:school.location.Longitude}, {lat: lat, lon: lon});                        
             app.locationService.viewModel.set("school", school);
             app.locationService.viewModel.school.set("isLoaded", true);
             app.locationService.viewModel.onNavigateHome.apply(app.locationService.viewModel, []);
-            
-            /*map.addMarker({
-            lat: lat,
-            lng: lon,
-            title: school.schoolName,
-            click: function(e) {
-            //alert('You clicked in this marker');
-            }
-            });*/
-            
             kendo.bind(e.view.element, app.locationService.viewModel, kendo.mobile.ui);
-            
-            /*dataContext.setWatchCallback(function(args) {
-            var position = args[0];
-            if (position) {                                  
-            savePosition(app.Friends.currentUser.data.Id, position); 
-            console.log('Position for friend received and processed');
-            }
-            });            
-            
-            app.locationService.viewModel.friend.set('isLoaded', true);          
-            
-            function savePosition(userId, position) {                            
-            // Save current user location and update connected user location
-            return dataContext.savePosition(userId, position).then(function(data) { 
-            app.Friends.currentUser.set('LastLocation', { latitude: position.coords.latitude, 'longitude': position.coords.longitude });                
-            console.log('savePosition: ' + JSON.stringify(data));
-            if (data.result.length > 0) {                        
-            for (var i = 0; i < data.result.length; i++) {                                                      
-            if (app.locationService.viewModel.friend.Id === data.result[i].LocationInfo.UserId) {
-            app.locationService.viewModel.friend.set('FriendSessionId', data.result[i].FriendSessionId);
-            app.locationService.viewModel.friend.set('FriendSessionActive', data.result[i].Active);
-            app.locationService.viewModel.friend.set('LastLocation', data.result[i].LocationInfo.LastLocation);
-            app.locationService.viewModel.friend.set('ModifiedAt', data.result[i].LocationInfo.ModifiedAt);
-            app.locationService.viewModel.friend.set('Distance', data.result[i].LocationInfo.Distance);
-            app.locationService.viewModel.friend.set('RequestSent', data.result[i].RequestSent);
-            app.locationService.viewModel.friend.set('RequestReceived', data.result[i].RequestReceived);
-            app.locationService.viewModel.friend.set('CurrentUserLocation', app.Friends.currentUser.LastLocation);                                 
-            var friendPosition = new google.maps.LatLng(data.result[i].LocationInfo.LastLocation.latitude, data.result[i].LocationInfo.LastLocation.longitude);
-            // TODO find another place where do this
-            map.panTo(friendPosition);
-            app.locationService.viewModel._putMarker(friendPosition);
-            } else {
-            }
-            }                        
-            } else {
-            app.locationService.viewModel.friend.set('FriendSessionActive', false);
-            app.locationService.viewModel.friend.set('LastLocation', null);                    
-            app.locationService.viewModel.friend.set('Distance', 0);
-            app.locationService.viewModel.friend.set('RequestSent', false);
-            app.locationService.viewModel.friend.set('RequestReceived', false);
-            }
-            if (app.locationService.viewModel.friend.FriendSessionActive) {
-            app.locationService.viewModel.friend.set('showMap', true);	
-            } else {                        
-            app.locationService.viewModel.friend.set('showMap', false);	
-            }                    
-            kendo.bind($("#view-friend"), app.locationService.viewModel, kendo.mobile.ui);
-            }, function(error) {
-            console.log(JSON.stringify(error));
-            });
-            } */  
             
             if (!app.locationService.viewModel.get("isGoogleMapsInitialized")) {
                 return;
@@ -220,55 +158,9 @@
         },
 
         viewModel: new LocationViewModel(),
-        
-        sendFriendRequest: function() {   
-            if (app.locationService.viewModel.friend.FriendSessionActive === false) {
-                app.mobileApp.showLoading();
-                dataContext.createFriendSession(app.locationService.viewModel.friend.Id, function(data) {
-                    app.locationService.viewModel.friend.set('RequestSent', true);                                        
-                    app.mobileApp.hideLoading();                    
-                    kendo.bind($("#view-friend"), app.locationService.viewModel, kendo.mobile.ui);
-                }, function(error) {
-                    app.mobileApp.hideLoading();
-                    console.log(JSON.stringify(error));
-                });	            
-            }
-        },
-        
-        acceptFriendConnection: function () {
-            if (app.locationService.viewModel.friend.FriendSessionActive === false && app.locationService.viewModel.friend.FriendSessionId !== -1) {
-                app.mobileApp.showLoading();
-                dataContext.acceptFriendSessionRequest(app.locationService.viewModel.friend.FriendSessionId, function(data) {
-                    app.locationService.viewModel.friend.set('FriendSessionActive', true);
-                    app.locationService.viewModel.set('showMap', true);
-                    app.mobileApp.hideLoading();                
-                    kendo.bind($("#view-friend"), app.locationService.viewModel, kendo.mobile.ui);
-                    console.log(JSON.stringify(data.result));
-                }, function(error) {
-                    app.mobileApp.hideLoading();
-                    console.log(JSON.stringify(error));
-                });	
-            }  
-        },
-        
-        disconnectFriend: function() {
-            if (app.locationService.viewModel.friend.FriendSessionActive && app.locationService.viewModel.friend.FriendSessionId !== -1) {
-                app.mobileApp.showLoading();
-                dataContext.disconnectFriend(app.locationService.viewModel.friend.FriendSessionId, function(data) {
-                    app.locationService.viewModel.friend.set('FriendSessionActive', false);
-                    app.locationService.viewModel.friend.set('RequestSent', false);
-                    app.locationService.viewModel.friend.set('RequestReceived', false);
-                    app.locationService.viewModel.set('showMap', false);
-                    app.mobileApp.hideLoading();                
-                    kendo.bind($("#view-friend"), app.locationService.viewModel, kendo.mobile.ui);
-                    console.log(JSON.stringify('Friend disconnected: ' + data.result));
-                }, function(error) {
-                    app.mobileApp.hideLoading();
-                    console.log(JSON.stringify(error));
-                });	
-            }  
+        onActivitiesClick: function (){
+            app.mobileApp.navigate('views/activitiesView.html');
         }
-        
     };
 }
 )(window);
